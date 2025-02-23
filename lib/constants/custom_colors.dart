78ffa6faa8a0b9a2c13e929cb86239479e7ef43d
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-23 11:48:20 +0900

    AR-GunManの時の渋可愛い系の配色を元にしたカスタムカラーを追加

---
+++ lib/constants/custom_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class CustomColor {
+  static const Color blackSteel = Color.fromRGBO(8, 7, 6, 1);
+  static const Color paper = Color.fromRGBO(239, 239, 239, 1);
+  static const Color goldLeaf = Color.fromRGBO(209, 178, 128, 1);
+  static const Color silver = Color.fromRGBO(89, 77, 70, 1);
+  static const Color customLightBrown = Color.fromRGBO(110, 102, 94, 1);
+  static const Color customDarkBrown = Color.fromRGBO(85, 78, 72, 1);
+}
